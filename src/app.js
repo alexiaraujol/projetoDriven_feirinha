d3718556833de177de18a2a6778a184c2f3cd8fe
alexiaraujol <151393882+alexiaraujol@users.noreply.github.com>
2025-03-28 15:33:54 -0300

    Consigo acessar o id da minha feirinha

--- src/app.js
+++ src/app.js
@@ -4,32 +4,44 @@ import cors from 'cors';
 const app = express();
 app.use(cors());
 
+const feirinha = [
+    {
+        id: 1,
+        name: "Maçã",
+        quantity: 1,
+        type: "Fruta",
+    },
+    {
+        id: 2,
+        name: "Uva",
+        quantity: 4,
+        type: "Fruta",
+    },
+    {
+        id: 1,
+        name: "Banana",
+        quantity: 5,
+        type: "Fruta",
+    },
+]
+
 app.get("/feirinha", (req, res) => {
-    const feirinha = [
-        {
-            id: 1, 
-            name: "Maçã",
-            quantity: 1,
-            type: "Fruta",
-        },
-        {
-            id: 2, 
-            name: "Uva",
-            quantity: 4,
-            type: "Fruta",
-        },
-        {
-            id: 1, 
-            name: "Banana",
-            quantity: 5,
-            type: "Fruta",
-        },
-    ]
 
     res.send(feirinha);
 
 })
 
+app.get("/feirinha/:id", (req, res) => {
+    const  id  = req.params.id;
+    const feirinhaItem = feirinha.find(item => {
+        return item.id == Number(id);
+    })
+    res.send(feirinhaItem);
+
+
+
+});
+
 app.listen(5000, () => {
     console.log("Servidor rodando na porta 5000")
-})
+});
