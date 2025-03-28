e67b9a3bd06396b35e733babed67ad60754f31b0
alexiaraujol <151393882+alexiaraujol@users.noreply.github.com>
2025-03-28 16:33:13 -0300

    Já é possivel adicionar itens a lista

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-import express from 'express';
+import express, { json } from 'express';
 import cors from 'cors';
 
 const app = express();
 app.use(cors());
+app.use(json());
 
-const feirinha = [
+const itemCompras = [
     {
         id: 1,
         name: "Maçã",
@@ -17,30 +18,45 @@ const feirinha = [
         quantity: 4,
         type: "Fruta",
     },
-    {
-        id: 1,
-        name: "Banana",
-        quantity: 5,
-        type: "Fruta",
-    },
 ]
 
 app.get("/feirinha", (req, res) => {
 
-    res.send(feirinha);
+    res.send(itemCompras);
 
 })
 
 app.get("/feirinha/:id", (req, res) => {
-    const  id  = req.params.id;
-    const feirinhaItem = feirinha.find(item => {
+    const id = req.params.id;
+    const buscarItem = itemCompras.find(item => {
         return item.id == Number(id);
     })
-    res.send(feirinhaItem);
-
-
-
+    res.send(buscarItem);
 });
+
+app.post("/feirinha", (req, res) => {
+    const itemAdicionado = req.body;
+    const { name, quantity, type } = itemAdicionado;
+
+    if (!name || !quantity || !type) {
+        return res.status(422).send("Preencha todos os campos!!")
+    }
+    if (typeof name !== "string" || typeof type !== "string") {
+        return res.status(400).send("Nome e tipo devem ser do tipo string!!")
+    }
+    if (quantity <= 0) {
+        return res.status(400).send("Quantidade deve ser maior que zero!!")
+    }
+    if (itemCompras.find(item => item.name === name)) {
+        return res.status(409 ).send("Esse item já existe na lista!!")
+    }    
+    itemCompras.push({
+        id: itemCompras.length + 1,
+        ...itemAdicionado
+    });
+
+    res.status(201).send("Seu item foi adicionado com sucesso!!")
+})
 
 app.listen(5000, () => {
     console.log("Servidor rodando na porta 5000")
